4bc2b9248b7c7399620252e4650cdd802df12f6e
Shabeeba Banu <110821724+ShabeebaBanu@users.noreply.github.com>
2024-09-10 01:16:57 +0530

    Delete Backend directory

--- Backend/src/main/java/bugBust/transitgo/repository/BusRouteRepository.java
+++
@@ -1,7 +0,0 @@
-package bugBust.transitgo.repository;
-
-import bugBust.transitgo.model.BusRoute;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BusRouteRepository extends JpaRepository<BusRoute,Long> {
-}
